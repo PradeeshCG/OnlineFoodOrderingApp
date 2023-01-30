44616ba4f90ed749f43abbc0059c3e277ee9dc30
G <pradeesh.g@capgemini.com>
2023-01-30 17:31:32 +0530

    onlinefoodorderingapplication

---
+++ com.cg.onlinefoodorderingapplication/src/main/java/com/cg/onlinefoodorderingapplication/exception/Exception.java
@@ -0,0 +1,5 @@
+package com.cg.onlinefoodorderingapplication.exception;
+
+public class Exception {
+
+}
---
+++ com.cg.onlinefoodorderingapplication/src/main/java/com/cg/onlinefoodorderingapplication/service/CustomerService.java
@@ -0,0 +1,5 @@
+package com.cg.onlinefoodorderingapplication.service;
+
+public class CustomerService {
+
+}
