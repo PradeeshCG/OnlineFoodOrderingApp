8311137bde2703a1544ae6106ed0e24c4c7c3b1a
Umadevi55 <122590184+Umadevi55@users.noreply.github.com>
2023-01-31 22:07:07 +0530

    Update ItemService.java

--- com.cg.onlinefoodorderingapplication/src/main/java/com/cg/onlinefoodorderingapplication/service/ItemService.java
+++ com.cg.onlinefoodorderingapplication/src/main/java/com/cg/onlinefoodorderingapplication/service/ItemService.java
@@ -1,5 +1,24 @@
 package com.cg.onlinefoodorderingapplication.service;
 
-public class ItemService {
+import java.util.List;
 
+import org.springframework.http.ResponseEntity;
+
+import com.cg.onlinefoodorderingapplication.entity.Item;
+import com.cg.onlinefoodorderingapplication.exception.ResourceNotFoundException;
+
+public interface IItemService {
+	public List<Item> getMenuByRestaurantId(Long restaurantId);
+
+	public List<Item> getMenu();
+
+	public Item saveItemByRestaurantId(Long restaurantId, Item item) throws ResourceNotFoundException;
+
+	public Item createItem(Item item);
+
+	public ResponseEntity<Item> getItemById(Long itemId) throws ResourceNotFoundException;
+
+	public ResponseEntity<Item> updateItem(Long itemId, Item itemDetails) throws ResourceNotFoundException;
+
+	public ResponseEntity<?> deleteItem(Long restaurantId, Long itemId) throws ResourceNotFoundException;
 }
